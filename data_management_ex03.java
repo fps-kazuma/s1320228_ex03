cfe65b8d43ed35eb6f5b3d2ee6443784431430cb
fps-kazuma <kazu.cosaka@gmail.com>
2025-06-27 18:09:51 +0900

    Won/lost message modified

--- data_management_ex03.java
+++ data_management_ex03.java
@@ -17,9 +17,9 @@ class Main{
     System.out.println("Die 2 : "+ n2 );
     System.out.println("Total value: "+ total);
     if(total>7){
-        System.out.println("You won!");
+        System.out.println(name +" won!");
     } else {
-        System.out.println("You lost.");
+        System.out.println(name +" lost.");
     }
   }
 }
